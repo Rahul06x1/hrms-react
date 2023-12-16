2d7b9e117be70ab48fd0506452c94417e3d7e638
Rahul Rajeev <rahulrajeev2k1@gmail.com>
2023-12-16 10:53:11 +0530

    Implement routers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/header';
+import Home from './components/home/home';
+import Employee from './components/employee/employee';
+import About from './components/about/about';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/employee" element={<Employee />} /> 
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
