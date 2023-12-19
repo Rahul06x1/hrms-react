29054bd8901f66d717dc1bd6cd65d1692dac9e4b
Rahul Rajeev <rahulrajeev2k1@gmail.com>
2023-12-19 17:03:48 +0530

    Implement 404 page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import Home from './components/home/home';
 import Employee from './components/employee/Employee';
 import About from './components/about/about';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Sidebar from './components/Siderbar/Sidebar';
+import NotFound from './components/notFound/notFound';
 import "./scss/volt.scss";
 
 function App() {
@@ -15,6 +16,8 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/employee" element={<Employee />} />
             <Route path="/about" element={<About />} />
+            <Route path="/404" element={<NotFound />} />
+            <Route path='*' element={<Navigate to={'/404'} />} />
           </Routes>
         </main>
     </div>
