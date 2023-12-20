b49f84cb92f3c3bca9aec622c57ae6e2123b20ec
Rahul Rajeev <rahulrajeev2k1@gmail.com>
2023-12-20 14:06:22 +0530

    Name function NotFound

--- src/components/notFound/notFound.js
+++ src/components/notFound/notFound.js
@@ -10,7 +10,7 @@ import { Routes } from "../../routes";
 import NotFoundImage from "../../assets/img/illustrations/404.svg";
 
 
-export default () => {
+export default function NotFound() {
   return (
     <main>
       <section className="vh-100 d-flex align-items-center justify-content-center">
