5d3655ece273fdf35e6c0efea96427d489d6eee0
Rahul Rajeev <rahulrajeev2k1@gmail.com>
2023-12-20 14:03:02 +0530

    Implement react strict mode

--- src/index.js
+++ src/index.js
@@ -9,11 +9,13 @@ import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <React.StrictMode>
     <Provider store={store}>
       <Router>
         <App />
       </Router>
     </Provider>
+    </React.StrictMode>
 );
 
 reportWebVitals();
