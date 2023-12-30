c8d578902d90b0391b029bb43803b38458c5d7cb
Rahul Rajeev <rahulrajeev2k1@gmail.com>
2023-12-30 16:07:53 +0530

    Remove deprecated library and unwanted variables

--- src/components/sideBar/sideBar.js
+++ src/components/sideBar/sideBar.js
@@ -2,10 +2,9 @@
 import React, { useState } from "react";
 import SimpleBar from 'simplebar-react';
 import { useLocation } from "react-router-dom";
-import { CSSTransition } from 'react-transition-group';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInfo, faTimes, faUser, faHome } from "@fortawesome/free-solid-svg-icons";
-import { Nav, Badge, Image, Button, Navbar } from 'react-bootstrap';
+import { Nav, Image, Button, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 import { Routes } from "../../routes";
@@ -21,23 +20,17 @@ export default function Sidebar() {
 
 
   const NavItem = (props) => {
-    const { title, link, external, target, icon, image, badgeText, badgeBg = "secondary", badgeColor = "primary" } = props;
-    const classNames = badgeText ? "d-flex justify-content-start align-items-center justify-content-between" : "";
+    const { title, link, icon } = props;
     const navItemClassName = link === pathname ? "active" : "";
-    const linkProps = external ? { href: link } : { as: Link, to: link };
+    const linkProps = { as: Link, to: link };
 
     return (
       <Nav.Item className={navItemClassName} onClick={() => setShow(false)}>
-        <Nav.Link {...linkProps} target={target} className={classNames}>
+        <Nav.Link {...linkProps} >
           <span>
             {icon ? <span className="sidebar-icon"><FontAwesomeIcon icon={icon} /> </span> : null}
-            {image ? <Image src={image} width={20} height={20} className="sidebar-icon svg-icon" /> : null}
-
             <span className="sidebar-text">{title}</span>
           </span>
-          {badgeText ? (
-            <Badge pill bg={badgeBg} text={badgeColor} className="badge-md notification-count ms-2">{badgeText}</Badge>
-          ) : null}
         </Nav.Link>
       </Nav.Item>
     );
@@ -53,27 +46,25 @@ export default function Sidebar() {
           <span className="navbar-toggler-icon" />
         </Navbar.Toggle>
       </Navbar>
-      <CSSTransition timeout={300} in={show} classNames="sidebar-transition">
-        <SimpleBar className={`collapse ${showClass} sidebar d-md-block bg-primary text-white`}>
-          <div className="sidebar-inner px-4 pt-3">
-            <div className="user-card d-flex d-md-none align-items-center justify-content-between justify-content-md-center pb-4">
-              <div className="d-flex align-items-center">
-                <div className="d-block">
-                  <h2>HRMS</h2>
-                </div>
+      <SimpleBar className={`collapse ${showClass} sidebar d-md-block bg-primary text-white`}>
+        <div className="sidebar-inner px-4 pt-3">
+          <div className="user-card d-flex d-md-none align-items-center justify-content-between justify-content-md-center pb-4">
+            <div className="d-flex align-items-center">
+              <div className="d-block">
+                <h2>HRMS</h2>
               </div>
-              <Nav.Link className="collapse-close d-md-none" onClick={onCollapse}>
-                <FontAwesomeIcon icon={faTimes} />
-              </Nav.Link>
             </div>
-            <Nav className="flex-column pt-3 pt-md-0">
-              <NavItem title="HRMS" link={Routes.Home.path} icon={faHome} />
-              <NavItem title="Employee" link={Routes.Employee.path} icon={faUser} />
-              <NavItem title="About" link={Routes.About.path} icon={faInfo} />
-            </Nav>
+            <Nav.Link className="collapse-close d-md-none" onClick={onCollapse}>
+              <FontAwesomeIcon icon={faTimes} />
+            </Nav.Link>
           </div>
-        </SimpleBar>
-      </CSSTransition>
+          <Nav className="flex-column pt-3 pt-md-0">
+            <NavItem title="HRMS" link={Routes.Home.path} icon={faHome} />
+            <NavItem title="Employee" link={Routes.Employee.path} icon={faUser} />
+            <NavItem title="About" link={Routes.About.path} icon={faInfo} />
+          </Nav>
+        </div>
+      </SimpleBar>
     </>
   );
 };
